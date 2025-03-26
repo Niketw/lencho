7986b9b65ef6f24368c597c872c04351abd6eb28
Aniket Inamdar <asinamdar2024@gmail.com>
2025-03-26 18:01:34 +0530

    added back button

--- lib/widgets/irrigation/irrigation_result_widget.dart
+++ lib/widgets/irrigation/irrigation_result_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class IrrigationResultWidget extends StatelessWidget {
   final double prediction;
@@ -8,111 +9,140 @@ class IrrigationResultWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: 100,
-      left: 20,
-      right: 20,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFFFFF4BE), // Light yellow
-              Color(0xFFACE268), // Light green
-            ],
-          ),
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0xFF2D5A27).withOpacity(0.15),
-              blurRadius: 10,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Row(
-              children: [
-                Icon(
-                  Icons.water_drop,
-                  color: Color(0xFF2D5A27),
-                  size: 24,
+    return Stack(
+      children: [
+        // Back Button
+        Positioned(
+          top: 40,
+          left: 20,
+          child: GestureDetector(
+            onTap: () => Get.back(),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.9),
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: const Color(0xFF2D5A27),
+                  width: 1,
                 ),
-                SizedBox(width: 12),
-                Text(
-                  "Irrigation Required",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xFF2D5A27),
-                    letterSpacing: 0.5,
-                  ),
+              ),
+              child: const Icon(
+                Icons.arrow_back,
+                color: Color(0xFF2D5A27),
+                size: 24,
+              ),
+            ),
+          ),
+        ),
+        // Existing Result Widget
+        Positioned(
+          top: 100,
+          left: 20,
+          right: 20,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFFFFF4BE), // Light yellow
+                  Color(0xFFACE268), // Light green
+                ],
+              ),
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFF2D5A27).withOpacity(0.15),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
                 ),
               ],
             ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const Row(
                   children: [
+                    Icon(
+                      Icons.water_drop,
+                      color: Color(0xFF2D5A27),
+                      size: 24,
+                    ),
+                    SizedBox(width: 12),
                     Text(
-                      "${prediction.toStringAsFixed(2)} cm",
-                      style: const TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                      "Irrigation Required",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
                         color: Color(0xFF2D5A27),
                         letterSpacing: 0.5,
                       ),
                     ),
-                    const Text(
-                      "Recommended water level",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Color(0xFF557153),
-                        letterSpacing: 0.3,
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${prediction.toStringAsFixed(2)} cm",
+                          style: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF2D5A27),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        const Text(
+                          "Recommended water level",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color(0xFF557153),
+                            letterSpacing: 0.3,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF2D5A27).withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.check_circle,
+                            color: Color(0xFF2D5A27),
+                            size: 16,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            "Optimal",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF2D5A27),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 6,
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF2D5A27).withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.check_circle,
-                        color: Color(0xFF2D5A27),
-                        size: 16,
-                      ),
-                      SizedBox(width: 4),
-                      Text(
-                        "Optimal",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF2D5A27),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
